6e43a02c5e37950930ad76879206a951112a9e8a
Rafael Cabral Coutinho <ed@tecnologiaenegocios.com.br>
2011-02-14 13:30:37 -0200

    Model::Principal can create the anonymous principal

--- spec/access_control/model/principal_spec.rb
+++ spec/access_control/model/principal_spec.rb
@@ -23,5 +23,31 @@ module AccessControl::Model
       Principal.securable?.should be_false
     end
 
+    describe "anonymous principal" do
+
+      let(:anonymous_principal) do
+        anonymous_subject_type = Principal.anonymous_subject_type
+        anonymous_subject_id = Principal.anonymous_subject_id
+        Principal.find_by_subject_type_and_subject_id(
+          anonymous_subject_type, anonymous_subject_id
+        )
+      end
+
+      it "creates the anonymous principal" do
+        Principal.create_anonymous_principal!
+      end
+
+      it "can return the anonymous principal object" do
+        Principal.create_anonymous_principal!
+        Principal.anonymous.should == anonymous_principal
+      end
+
+      it "can return the anonymous principal id" do
+        Principal.create_anonymous_principal!
+        Principal.anonymous_id.should == anonymous_principal.id
+      end
+
+    end
+
   end
 end
