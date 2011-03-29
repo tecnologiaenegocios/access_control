aba630ca9858996ba79664cff557dbfebc7b5a91
Rafael Cabral Coutinho <ed@tecnologiaenegocios.com.br>
2011-03-29 18:47:51 -0300

    Method to return management assignments for UI

--- lib/access_control/assignment.rb
+++ lib/access_control/assignment.rb
@@ -7,5 +7,20 @@ module AccessControl
     def self.securable?
       false
     end
+
+    def self.items_for_management(node, roles)
+      all(
+        :conditions => {:node_id => node.id, :role_id => roles.map(&:id)}
+      ).group_by{|a| a.principal_id}.inject({}) do |r, (p_id, assigns)|
+        r[p_id] = roles.map do |role|
+          if assignment = assigns.detect{|a| a.role_id == role.id}
+            next assignment
+          end
+          Assignment.new(:role_id => role.id, :node_id => node.id,
+                         :principal_id => p_id)
+        end
+        r
+      end
+    end
   end
 end
--- spec/access_control/assignment_spec.rb
+++ spec/access_control/assignment_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 module AccessControl
+
   describe Assignment do
     it "can be created with valid attributes" do
       Assignment.create!(
@@ -13,4 +14,78 @@ module AccessControl
       Assignment.securable?.should be_false
     end
   end
+
+  describe "assignments for management" do
+
+    before do
+      Node.create_global_node!
+      roles = [
+        @role1 = Role.create!(:name => 'role1'),
+        @role2 = Role.create!(:name => 'role2'),
+        @role3 = Role.create!(:name => 'role3')
+      ]
+      principals = [
+        @principal1 = Principal.create!(
+          :subject_type => 'SubjectType',
+          :subject_id => 0
+        ),
+        @principal2 = Principal.create!(
+          :subject_type => 'SubjectType',
+          :subject_id => 1
+        ),
+        @principal3 = Principal.create!(
+          :subject_type => 'SubjectType',
+          :subject_id => 2
+        ),
+        @principal4 = Principal.create!(
+          :subject_type => 'SubjectType',
+          :subject_id => 3
+        )
+      ]
+      @node = Node.create!(:securable_type => 'SecurableType',
+                           :securable_id => 0)
+      @item1 = Assignment.create!(
+        :node => @node, :principal => @principal1, :role => @role1
+      )
+      @item2 = Assignment.create!(
+        :node => @node, :principal => @principal2, :role => @role2
+      )
+      Assignment.create!(:node_id => @node.id + 1,
+                         :principal => @principal4, :role => @role1)
+      @items = Assignment.items_for_management(@node, roles)
+    end
+
+    it "return one key for each different principal id with some role "\
+       "assigned to the node" do
+      @items.size.should == 2
+    end
+
+    it "returns one value for each role passed in" do
+      @items[@principal1.id].size.should == 3
+      @items[@principal2.id].size.should == 3
+    end
+
+    it "returns assignments that already exist for the node" do
+      @items[@principal1.id].first.should == @item1
+      @items[@principal2.id].second.should == @item2
+    end
+
+    it "returns new assignments when there's no assignment created" do
+      @items[@principal1.id].second.should be_new_record
+      @items[@principal1.id].second.node_id.should == @node.id
+      @items[@principal1.id].second.role_id.should == @role2.id
+      @items[@principal1.id].third.should be_new_record
+      @items[@principal1.id].third.node_id.should == @node.id
+      @items[@principal1.id].third.role_id.should == @role3.id
+
+      @items[@principal2.id].first.should be_new_record
+      @items[@principal2.id].first.node_id.should == @node.id
+      @items[@principal2.id].first.role_id.should == @role1.id
+      @items[@principal2.id].third.should be_new_record
+      @items[@principal2.id].third.node_id.should == @node.id
+      @items[@principal2.id].third.role_id.should == @role3.id
+    end
+
+  end
+
 end
