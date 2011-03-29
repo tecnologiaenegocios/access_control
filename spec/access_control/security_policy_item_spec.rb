710434a36a102448b156a0760c17a412f097a326
Rafael Cabral Coutinho <ed@tecnologiaenegocios.com.br>
2011-03-29 15:46:19 -0300

    Fix test description

--- spec/access_control/security_policy_item_spec.rb
+++ spec/access_control/security_policy_item_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module AccessControl
   describe SecurityPolicyItem do
 
-    it "cannot be wrapped by a security proxy" do
+    it "is not securable" do
       SecurityPolicyItem.securable?.should be_false
     end
 
