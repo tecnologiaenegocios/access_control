6f79a4c8cef5eabd50d420fc8aab18d8482c20e1
Rafael Cabral Coutinho <rcabralc@tecnologiaenegocios.com.br>
2011-08-23 20:38:01 -0300

    No, don't include Inheritance with Restriction yet

--- lib/access_control/restriction.rb
+++ lib/access_control/restriction.rb
@@ -1,10 +1,7 @@
-require 'access_control/inheritance'
 require 'access_control/restricter'
 
 module AccessControl
   module Restriction
-
-    include Inheritance
 
     def self.included(base)
       base.extend(ClassMethods)
