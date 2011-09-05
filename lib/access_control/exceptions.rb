0621b4e039d482e9af4ba522463e57754b7c0a0e
Rafael Cabral Coutinho <rcabralc@tecnologiaenegocios.com.br>
2011-09-05 19:15:34 -0300

    Cleaned up exceptions

--- lib/access_control/exceptions.rb
+++ lib/access_control/exceptions.rb
@@ -3,25 +3,13 @@ module AccessControl
   class Unauthorized < StandardError
   end
 
-  class ParentError < StandardError
-  end
-
   class NoGlobalNode < StandardError
-  end
-
-  class CannotWrapUnsecurableObject < StandardError
   end
 
   class NoContextError < StandardError
   end
 
   class InvalidInheritage < StandardError
-  end
-
-  class InvalidPropagation < StandardError
-  end
-
-  class MissingPropagation < StandardError
   end
 
   class MissingPermissionDeclaration < StandardError
