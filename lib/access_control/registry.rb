b1a9267f763534bf79d418dcab1db6c5c070fed5
Rafael Cabral Coutinho <rcabralc@tecnologiaenegocios.com.br>
2011-09-12 21:33:57 -0300

    Registry API changes
    - Added .query method
    - .all_with_options renamed to .all_with_metadata

--- lib/access_control/registry.rb
+++ lib/access_control/registry.rb
@@ -1,81 +1,102 @@
 require 'access_control/util'
 
 module AccessControl
-  module Registry
+  class RegistryFactory
 
-    class << self
+    def initialize
+      clear_registry
+    end
 
-      def clear_registry
-        @permissions = nil
-        @permissions_with_options = nil
+    def clear_registry
+      @permissions = Set.new
+      @permissions_with_metadata = Hash.new{|h, k| h[k.to_s] = Set.new }
+      @permissions_by_metadata = {}
+    end
+
+    def register *args
+      metadata = args.extract_options!.dup
+      Util.make_set_from_args(*args).each do |permission|
+        register_permission(permission)
+        register_metadata(metadata, permission)
       end
+    end
 
-      def register *args
-        options = args.extract_options!
-        Util.make_set_from_args(*args).each do |p|
-          permissions << p
-          permissions_with_options[p] << options.dup
-        end
+    def all
+      @permissions
+    end
+
+    def all_with_metadata
+      @permissions_with_metadata
+    end
+
+    def register_undeclared_permissions
+      register([
+        'grant_roles',
+        'share_own_roles',
+        'change_inheritance_blocking'
+      ])
+    end
+
+    def query(*criteria)
+      return all if criteria.empty?
+      criteria.map{|criterion| permissions_matching(criterion)}.inject(&:|)
+    end
+
+    def load_all_controllers
+      Dir[Rails.root + 'app/controllers/**/*.rb'].each do |path|
+        load_top_level_constant(path)
       end
+    end
 
-      def all
-        permissions
+    def load_all_models
+      Dir[Rails.root + 'app/models/**/*.rb'].each do |path|
+        load_top_level_constant(path)
       end
+    end
 
-      def all_with_options
-        permissions_with_options
+    def load_all_permissions_from_config
+      AccessControl.config.register_permissions
+    end
+
+  private
+
+    def register_permission(permission)
+      all << permission
+    end
+
+    def register_metadata(metadata, permission)
+      all_with_metadata[permission] << metadata
+      metadata.each do |key, value|
+        query_permissions(key, value) << permission
       end
+    end
 
-      def register_undeclared_permissions
-        register([
-          'grant_roles',
-          'share_own_roles',
-          'change_inheritance_blocking'
-        ])
-      end
+    def permissions_matching(criterion)
+      return all if criterion.empty?
+      criterion.map{|key, value| query_permissions(key, value)}.inject(&:&)
+    end
 
-      def load_all_controllers
-        Dir[Rails.root + 'app/controllers/**/*.rb'].each do |path|
-          load_top_level_constant(path)
-        end
-      end
+    def query_permissions(key, value)
+      @permissions_by_metadata[[key, value]] ||= Set.new
+    end
 
-      def load_all_models
-        Dir[Rails.root + 'app/models/**/*.rb'].each do |path|
-          load_top_level_constant(path)
-        end
-      end
-
-      def load_all_permissions_from_config
-        AccessControl.config.register_permissions
-      end
-
-    private
-
-      def load_top_level_constant filename
-        # We can't simply load or require the file by its filename because of
-        # the handling of cached classes in Rails, so we deduce their class
-        # name from the filename and get the constant (a.k.a. the top level
-        # class).  If the class wasn't loaded yet, ActiveSupport::Dependencies
-        # will get it from the source file based on the its name anyway, taking
-        # into consideration the cache_class config option.
-        filename.
-          gsub(Rails.root + 'app/models/', '').
-          gsub(Rails.root + 'app/controllers/', '').
-          gsub(/\.rb$/, '').
-          camelize.
-          constantize
-      end
-
-      def permissions
-        @permissions ||= Set.new
-      end
-
-      def permissions_with_options
-        @permissions_with_options ||= Hash.new{|h, k| h[k.to_s] = Set.new }
-      end
-
+    def load_top_level_constant filename
+      # We can't simply load or require the file by its filename because of
+      # the handling of cached classes in Rails, so we deduce their class
+      # name from the filename and get the constant (a.k.a. the top level
+      # class).  If the class wasn't loaded yet, ActiveSupport::Dependencies
+      # will get it from the source file based on the its name anyway, taking
+      # into consideration the cache_class config option.
+      filename.
+        gsub(Rails.root + 'app/models/', '').
+        gsub(Rails.root + 'app/controllers/', '').
+        gsub(/\.rb$/, '').
+        camelize.
+        constantize
     end
 
   end
+
+  Registry = RegistryFactory.new
+
 end
--- spec/access_control/registry_spec.rb
+++ spec/access_control/registry_spec.rb
@@ -13,107 +13,202 @@ module AccessControl
 
       it "registers permissions through self.register" do
         Registry.register('some permission')
-        Registry.all.should include('some permission')
-        Registry.all.size.should == 1
+        Registry.all.should == Set.new(['some permission'])
       end
 
       it "accepts an argument list of permissions to register" do
         Registry.register('some permission', 'another permission')
-        Registry.all.should include('some permission')
-        Registry.all.should include('another permission')
-        Registry.all.size.should == 2
+        Registry.all.should == Set.new(['some permission',
+                                        'another permission'])
       end
 
       it "accepts an array of permissions to register" do
         Registry.register(['some permission', 'another permission'])
-        Registry.all.should include('some permission')
-        Registry.all.should include('another permission')
-        Registry.all.size.should == 2
+        Registry.all.should == Set.new(['some permission',
+                                        'another permission'])
       end
 
       it "accepts a set of permissions to register" do
         Registry.register(
           Set.new(['some permission', 'another permission'])
         )
-        Registry.all.should include('some permission')
-        Registry.all.should include('another permission')
-        Registry.all.size.should == 2
+        Registry.all.should == Set.new(['some permission',
+                                        'another permission'])
       end
 
-      describe "with options" do
+      describe "with metadata" do
 
-        # The options stuff: this never overwrites any option, and should
-        # preserve the final result no matter the order the permissions are
-        # registered.
+        # Metadata are a way to assign information into a permission when it
+        # gets registered.  This allows one to query permissions later
+        # according to the data that was registered with them.
         #
-        # Permissions registered with exactly the same options will be
-        # registered once.  Any different option will generate two or more
-        # entries.  Anyway, the result of `all_with_options` is a hash, keyed
-        # by the permission name, and the values are enumerables with unique
-        # values, and the order should not matter for any purpose (the
-        # implementation is allowed to use a Set in this case as the value).
+        # Suppose that one wants to register a permission for a method in a
+        # specific class (say, a field in a model).  One way to do this and
+        # still keep track of the registration is by assigning a metadata for
+        # the permission.  Example:
+        #
+        #   Registry.register('read_field_name_in_my_model',
+        #                     :model => MyModel.name,
+        #                     :method => :field_name)
+        #
+        # Later the permission could be queried by { :model => MyModel.name,
+        # :method => :field_name }.  The metadata is the Hash passed as the
+        # last argument to .register.  Note that a metadata cannot hold model
+        # classes directly (although most classes can be held) because of the
+        # caching/discarding ActiveSupport::Dependencies does, and so the name
+        # of the class is used (both when registering and when querying).
+        #
+        # A metadata key/value pair never overwrites another:
+        #
+        #   Register.register('a_permission', :key => 'value')
+        #   Register.register('a_permission', :key => 'other_value')
+        #
+        # The above registrations result in two different metadata for the same
+        # permission.  This permission can be queryied with any of the
+        # following:
+        #
+        #   { :key => 'value' }
+        #   { :key => 'other_value' }
+        #
+        # This example group is concerned only with the .register method when
+        # using a hash for registering, and the assertions are done inspecting
+        # the return of .all_with_metadata and .all.
 
         it "accepts options" do
-          Registry.register('some permission', :option => 'Value')
+          Registry.register('some permission', :key => 'value')
         end
 
         it "registers when using options" do
-          Registry.register('some permission', :option => 'Value')
-          Registry.all_with_options['some permission'].
-            should include(:option => 'Value')
+          Registry.register('some permission', :key => 'value')
+          Registry.all_with_metadata.should == {
+            'some permission' => Set.new([{:key => 'value'}])
+          }
         end
 
         it "registers many permissions using the same options" do
-          Registry.register('some permission', 'another permission',
-                            :option => 'Value')
-          r = Registry.all_with_options
-          r['some permission'].should include(:option => 'Value')
-          r['some permission'].size.should == 1
-          r['another permission'].should include(:option => 'Value')
-          r['another permission'].size.should == 1
-          r.size.should == 2
+          Registry.register('permission 1',
+                            'permission 2',
+                            :key => 'value')
+          Registry.all_with_metadata.should == {
+            'permission 1' => Set.new([{:key => 'value'}]),
+            'permission 2' => Set.new([{:key => 'value'}])
+          }
         end
 
         it "registers a permission with the same options once" do
-          Registry.register('some permission', :option => 'Value')
-          Registry.register('some permission', :option => 'Value')
-          r = Registry.all_with_options
-          r['some permission'].should include(:option => 'Value')
-          r['some permission'].size.should == 1
-          r.size.should == 1
+          Registry.register('some permission', :key => 'value')
+          Registry.register('some permission', :key => 'value')
+          r = Registry.all_with_metadata
+          r.should == { 'some permission' => Set.new([{:key => 'value'}]) }
         end
 
         it "registers a permission with all different options passed" do
-          Registry.register('some permission', :option => 'Value1',
-                            :other_option => 'Value2')
-          Registry.register('some permission', :option => 'Value2')
-          r = Registry.all_with_options
-          r['some permission'].should include(:option => 'Value1',
-                                              :other_option => 'Value2')
-          r['some permission'].should include(:option => 'Value2')
-          r['some permission'].size.should == 2
-          r.size.should == 1
+          Registry.register('some permission',
+                            :key_1 => 'value 1',
+                            :key_2 => 'value 2')
+          Registry.register('some permission', :key_1 => 'value 2')
+          Registry.all_with_metadata.should == {
+            'some permission' => Set.new([
+              {:key_1 => 'value 1', :key_2 => 'value 2'},
+              {:key_1 => 'value 2'}
+            ])
+          }
         end
 
         it "registers an empty hash if no options are passed" do
           Registry.register('some permission')
-          Registry.all_with_options['some permission'].should include({})
-          Registry.all_with_options['some permission'].size.should == 1
-          Registry.all_with_options.size.should == 1
+          Registry.all_with_metadata.should == {
+            'some permission' => Set.new([{}])
+          }
         end
 
-        it "registers when using options and permission in #all" do
-          Registry.register('some permission', :option => 'Value')
-          Registry.all.should include('some permission')
-          Registry.all.size.should == 1
+        it "registers when using options and permission in .all" do
+          Registry.register('some permission', :key => 'value')
+          Registry.all.should == Set.new(['some permission'])
         end
 
-        it "registers the same permission with different options but #all "\
-          "returns the permission only once" do
-          Registry.register('some permission', :option => 'Value1')
-          Registry.register('some permission', :option => 'Value2')
-          Registry.all.should include('some permission')
-          Registry.all.size.should == 1
+        it "registers the same permission with different options but .all "\
+           "returns the permission only once" do
+          Registry.register('some permission', :key => 'value 1')
+          Registry.register('some permission', :key => 'value 2')
+          Registry.all.should == Set.new(['some permission'])
+        end
+
+      end
+
+    end
+
+    describe ".query" do
+
+      # Querying is done against the metadata registered with a permission.
+
+      before do
+        Registry.
+          register('permission 1', :key_1 => 'value 1', :key_2 => 'value 2')
+        Registry.
+          register('permission 1', :key_1 => 'unique value')
+        Registry.
+          register('permission 2', :key_1 => 'value 1')
+        Registry.
+          register('permission 2', :key_1 => 'other unique value')
+      end
+
+      it "returns all permissions if no criteria is passed" do
+        Registry.query.should == Registry.all
+      end
+
+      it "returns all permissions if an empty hash is passed" do
+        Registry.query({}).should == Registry.all
+      end
+
+      describe "with single criterion" do
+
+        # A criterion is a hash, whose keys/values must match the metadata of
+        # some permission in order of that being returned.
+
+        it "returns permissions matching key => value" do
+          results = Registry.query(:key_1 => 'unique value')
+          results.should include('permission 1')
+        end
+
+        it "doesn't return permissions for which key => value isn't valid" do
+          results = Registry.query(:key_1 => 'unique value')
+          results.should_not include('permission 2')
+        end
+
+        it "accepts multiple key/value pairs (performs AND operation)" do
+          results = Registry.query(:key_1 => 'value 1', :key_2 => 'value 2')
+          results.should include('permission 1')
+          results.should_not include('permission 2')
+        end
+
+        it "checks all metadata available in all registrations" do
+          results = Registry.query(:key_1 => 'unique value',
+                                   :key_2 => 'value 2')
+          results.should include('permission 1')
+          results.should_not include('permission 2')
+        end
+
+      end
+
+      describe "with multiple criteria" do
+
+        # Each argument is a criterion.
+
+        it "accepts multiple hashes (performs OR operation)" do
+          results = Registry.query({:key_1 => 'unique value'},
+                                   {:key_1 => 'other unique value'})
+          results.should include('permission 1')
+          results.should include('permission 2')
+        end
+
+        it "can combine AND and OR operations" do
+          results = Registry.query(
+            {:key_1 => 'unique value', :key_2 => 'value 2'},
+            {:key_1 => 'other unique value'}
+          )
+          results.should include('permission 1')
+          results.should include('permission 2')
         end
 
       end
@@ -186,7 +281,7 @@ module AccessControl
 
         it "registers '#{p}' with empty options" do
           Registry.register_undeclared_permissions
-          Registry.all_with_options[p].should include({})
+          Registry.all_with_metadata[p].should include({})
         end
 
       end
