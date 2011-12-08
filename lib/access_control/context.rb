a1f9d420c46cf5e942a462159188136c0ae1b747
Renato Riccieri <renato.riccieri@gmail.com>
2011-12-08 19:44:40 -0200

    Made Context use AccessControl::Node

--- lib/access_control/context.rb
+++ lib/access_control/context.rb
@@ -4,18 +4,16 @@ require 'access_control/parenter'
 module AccessControl
   class Context
 
+    attr_reader :nodes
     def initialize(item_or_collection)
-      @collection = Array(item_or_collection)
-    end
-
-    def nodes
-      Util.flat_set(@collection) { |item| nodes_relevant_to(item) }
+      collection = Array(item_or_collection)
+      @nodes     = Util.flat_set(collection, &method(:nodes_relevant_to))
     end
 
   private
 
     def nodes_relevant_to(item)
-      if owned_node = node_owned_by(item)
+      if owned_node = AccessControl::Node(item)
         Set[owned_node]
       else
         item_parents = Parenter.new(item).get
@@ -23,8 +21,5 @@ module AccessControl
       end
     end
 
-    def node_owned_by(item)
-      item.is_a?(Node) ? item : item.ac_node
-    end
   end
 end
