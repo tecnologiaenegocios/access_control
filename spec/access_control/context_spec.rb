09d6e03838d2c1d4d09db593daf8d25757d709fb
Renato Riccieri <renato.riccieri@gmail.com>
2011-12-16 16:52:25 -0200

    Made 'Context' specs follow the new 'Node' method rules

--- spec/access_control/context_spec.rb
+++ spec/access_control/context_spec.rb
@@ -5,10 +5,12 @@ module AccessControl
 
   describe Context do
 
+    let(:securable_class) { FakeSecurableClass.new }
+
     let(:node1) { stub_model(Node) }
     let(:node2) { stub_model(Node) }
-    let(:record1) { stub(:ac_node => node1) }
-    let(:record2) { stub(:ac_node => node2) }
+    let(:record1) { securable_class.new(:ac_node => node1) }
+    let(:record2) { securable_class.new(:ac_node => node2) }
 
     it "extracts a single node from, er, a node" do
       Context.new(node1).nodes.should == Set.new([node1])
@@ -34,19 +36,17 @@ module AccessControl
       end
 
       describe "if the object hasn't a node" do
+        let(:record1) { securable_class.new(:ac_node => nil) }
+        let(:record2) { securable_class.new(:ac_node => nil) }
 
-        let(:record1) { stub(:ac_node => nil) }
-        let(:record2) { stub(:ac_node => nil) }
-
-        let(:parent1) { Set[stub('parent 1', :ac_node => node1)] }
-        let(:parent2) { Set[stub('parent 2', :ac_node => node2)] }
+        let(:parent1) { securable_class.new(:ac_node => node1) }
+        let(:parent2) { securable_class.new(:ac_node => node2) }
 
         it "uses its parents (assumes that the object is a baby)" do
-          Parenter.stub(:parents_of).with(record1).and_return(parent1)
-          Parenter.stub(:parents_of).with(record2).and_return(parent2)
+          Parenter.stub(:parents_of).with(record1).and_return(Set[parent1])
+          Parenter.stub(:parents_of).with(record2).and_return(Set[parent2])
 
-          Context.new([record1, record2]).nodes.
-            should == Set[node1, node2]
+          Context.new([record1, record2]).nodes.should == Set[node1, node2]
         end
 
       end
