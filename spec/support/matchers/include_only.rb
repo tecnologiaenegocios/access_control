5c84fa4129168dbef6947d16440910cf121bf986
Renato Riccieri Santos Zannon <zannon@tecnologiaenegocios.com.br>
2012-01-03 17:04:31 -0200

    The method #size isn't defined for Enumerable, #count is

--- spec/support/matchers/include_only.rb
+++ spec/support/matchers/include_only.rb
@@ -4,7 +4,7 @@ Spec::Matchers.define :include_only do |*items|
     includes_all_items = items.all? do |item|
       target.include?(item)
     end
-    has_same_cardinality = items.size == target.size
+    has_same_cardinality = items.count == target.count
 
     includes_all_items and has_same_cardinality
   end
