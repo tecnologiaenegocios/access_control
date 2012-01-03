3a161eaca6a0eec7c34a0917ff6966f5ff7a3cdf
Renato Riccieri Santos Zannon <zannon@tecnologiaenegocios.com.br>
2012-01-03 18:27:42 -0200

    AccessControl::Ids cleaning and expansion

--- lib/access_control/ids.rb
+++ lib/access_control/ids.rb
@@ -9,30 +9,19 @@ module AccessControl
       scoped(:conditions => { :id => ids })
     end
 
-    def method_missing method_name, *args, &block
-      if foreign_key = belongs_to_association_ids_or_nil(method_name)
-        return select_values_of_column(foreign_key)
-      end
-      super
-    end
-
     def select_values_of_column(column_name)
-      connection.select_values(scoped(
-        :select => "DISTINCT #{quoted_table_name}.#{column_name}"
-      ).sql)
+      sql = column_sql(column_name)
+      connection.select_values(sql)
     end
 
-  private
-
-    def belongs_to_association_ids_or_nil(method_name)
-      if method_name.to_s.ends_with?('_ids')
-        if r = reflections[method_name.to_s.gsub(/_ids$/, '').to_sym]
-          if r.belongs_to?
-            return r.primary_key_name
-          end
-        end
-      end
+    def scoped_column(column_name)
+      scoped(:select => "DISTINCT #{quoted_table_name}.#{column_name}")
     end
+
+    def column_sql(column_name)
+      scoped_column(column_name).sql
+    end
+
   end
 end
 
