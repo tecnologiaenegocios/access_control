fef83524835af397d748a750b136043fa54f4a84
Rafael Cabral Coutinho <rcabralc@tecnologiaenegocios.com.br>
2012-01-06 19:21:28 -0200

    Persistency protection considered harmful (temporarily)

--- lib/access_control/persistency_protector.rb
+++ lib/access_control/persistency_protector.rb
@@ -3,34 +3,34 @@ module AccessControl
     class << self
 
       def track_parents(instance)
-        parents = Parenter.parents_of(instance)
-        set_tracked_parents(instance, parents)
+        # parents = Parenter.parents_of(instance)
+        # set_tracked_parents(instance, parents)
       end
 
       def verify_attachment!(instance)
-        existing_parents = get_tracked_parents(instance)
-        current_parents = Parenter.parents_of(instance)
-        added_parents = current_parents - existing_parents
-        permissions = permissions_for(:create, instance)
+        # existing_parents = get_tracked_parents(instance)
+        # current_parents = Parenter.parents_of(instance)
+        # added_parents = current_parents - existing_parents
+        # permissions = permissions_for(:create, instance)
 
-        added_parents.each do |new_parent|
-          manager.can!(permissions, new_parent)
-        end
+        # added_parents.each do |new_parent|
+        #   manager.can!(permissions, new_parent)
+        # end
       end
 
       def verify_detachment!(instance)
-        existing_parents = get_tracked_parents(instance)
-        current_parents = Parenter.parents_of(instance)
-        removed_parents = existing_parents - current_parents
-        permissions = permissions_for(:destroy, instance)
+        # existing_parents = get_tracked_parents(instance)
+        # current_parents = Parenter.parents_of(instance)
+        # removed_parents = existing_parents - current_parents
+        # permissions = permissions_for(:destroy, instance)
 
-        removed_parents.each do |old_parent|
-          manager.can!(permissions, old_parent)
-        end
+        # removed_parents.each do |old_parent|
+        #   manager.can!(permissions, old_parent)
+        # end
       end
 
       def verify_update!(instance)
-        manager.can!(permissions_for(:update, instance), instance)
+        # manager.can!(permissions_for(:update, instance), instance)
       end
 
     private
