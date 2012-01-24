65f7d871ed659e5a9cde5ef95862eb2facbc7a0b
Rafael Cabral Coutinho <rcabralc@tecnologiaenegocios.com.br>
2012-01-24 19:50:50 -0200

    Don't add unneeded child ids to propagation inserts

--- lib/access_control/assignment/persistent.rb
+++ lib/access_control/assignment/persistent.rb
@@ -9,8 +9,9 @@ module AccessControl
     class << self
       def propagate_to(assignments, node_id)
         propagate_descendants(ids_of(assignments),
-                              :from => node_ids_of(assignments),
-                              :to   => node_id)
+                              :from      => node_ids_of(assignments),
+                              :to        => node_id,
+                              :scoped_by => node_id)
       end
 
       def propagate_to_descendants(assignments, node_id)
@@ -58,40 +59,50 @@ module AccessControl
       end
 
       def propagate_descendants(source_ids, params)
-        parent_node_ids = params[:from]
-        child_node_ids  = params[:to]
+        parent_node_ids   = params[:from]
+        child_node_ids    = params[:to]
+        child_nodes_scope = params[:scoped_by]
 
-        copy(source_ids, :from => parent_node_ids, :to => child_node_ids)
+        descend(source_ids,
+                :from => parent_node_ids,
+                :scoped_by => child_nodes_scope)
         new_ids = propagated_ids_from(source_ids, :at => child_node_ids)
-        next_child_node_ids = child_node_ids_of(child_node_ids)
 
+        # Next levels are propagated always without scope.
+        next_child_node_ids = child_node_ids_of(child_node_ids)
         if next_child_node_ids.any?
           propagate_descendants(new_ids, :from => child_node_ids,
                                          :to   => next_child_node_ids)
         end
       end
 
-      def copy(source_ids, params)
-        parent_node_ids = params[:from]
-        child_node_ids  = params[:to]
+      def descend(source_ids, params)
+        parent_node_ids   = params[:from]
+        child_nodes_scope = params[:scoped_by]
 
-        combos = combinations_of(source_ids, :from => parent_node_ids,
-                                             :to => child_node_ids)
+        combos = combinations_of(source_ids,
+                                 :from      => parent_node_ids,
+                                 :scoped_by => child_nodes_scope)
 
         import([:parent_id, :role_id, :principal_id, :node_id], combos)
       end
 
       def combinations_of(source_ids, params)
-        parent_node_ids = params[:from]
-        child_node_ids  = params[:to]
+        parent_node_ids   = params[:from]
+        child_nodes_scope = params[:scoped_by]
+
+        filter_params = { :ac_parents__parent_id => parent_node_ids,
+                          :ac_assignments__id    => source_ids }
+
+        if child_nodes_scope
+          filter_params[:ac_parents__child_id] = child_nodes_scope
+        end
 
         select(:ac_assignments__id, :role_id, :principal_id,
                :ac_parents__child_id).
           join_table(:inner, :ac_parents,
                      :ac_assignments__node_id => :ac_parents__parent_id).
-          filter(:ac_parents__parent_id => parent_node_ids,
-                 :ac_parents__child_id  => child_node_ids,
-                 :ac_assignments__id    => source_ids)
+          filter(filter_params)
       end
 
       def propagated_ids_from(parent_ids, params)
