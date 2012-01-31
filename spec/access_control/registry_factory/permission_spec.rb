e08a52da2cb81d16ed4995a2983c1d750d779b5d
Rafael Cabral Coutinho <rcabralc@tecnologiaenegocios.com.br>
2012-01-31 18:09:23 -0200

    Ensure that permission keeps a reference to its attributes

--- spec/access_control/registry_factory/permission_spec.rb
+++ spec/access_control/registry_factory/permission_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 module AccessControl; class RegistryFactory;
   describe Permission do
 
+    subject { Permission.new }
+
     it "has a name" do
-      permission = Permission.new
-      permission.name.should == ''
+      subject.name.should == ''
     end
 
     it "can have its name set on initialization" do
@@ -14,18 +15,30 @@ module AccessControl; class RegistryFactory;
     end
 
     it "responds to :ac_methods and returns an empty set by default" do
-      permission = Permission.new
-      permission.ac_methods.should == Set.new
+      subject.ac_methods.should == Set.new
+    end
+
+    it "can have its :ac_methods modified" do
+      subject.ac_methods << 'foo'
+      subject.ac_methods.should include('foo')
     end
 
     it "responds to :ac_classes and returns an empty set by default" do
-      permission = Permission.new
-      permission.ac_classes.should == Set.new
+      subject.ac_classes.should == Set.new
+    end
+
+    it "can have its :ac_classes modified" do
+      subject.ac_classes << 'foo'
+      subject.ac_classes.should include('foo')
     end
 
     it "responds to :context_designator and returns an empty hash by default" do
-      permission = Permission.new
-      permission.context_designator.should == {}
+      subject.context_designator.should == {}
+    end
+
+    it "can have its :context_designator modified" do
+      subject.context_designator['foo'] = 'bar'
+      subject.context_designator['foo'].should == 'bar'
     end
 
   end
