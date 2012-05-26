21cec867ec84da728a2ad88bf3600e205dfa44fa
Renato Riccieri Santos Zannon <zannon@tecnologiaenegocios.com.br>
2012-05-26 15:01:00 -0300

    Add our first integration spec

---
+++ spec/integration/key_inheritance_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe "key-based inheritance" do
+  class Record < ActiveRecord::Base
+    include AccessControl::Securable
+
+    belongs_to :parent, :foreign_key => :record_id, :class_name => "Record"
+    inherits_permissions_from_key :record_id, :class_name => "Record"
+  end
+
+  class User < ActiveRecord::Base
+    include AccessControl::ActiveRecordSubject
+  end
+
+  let(:role) { AccessControl::Role.store(:name => "Role") }
+  let(:user) { User.create! }
+
+  before do
+    Record.requires_no_permissions!
+  end
+
+  let(:parent_record)  { Record.create! }
+  let(:child_record)   { Record.create!(:parent => parent_record) }
+
+  it "propagates roles from parent records to child record" do
+    role.assign_to(user, parent_record)
+    role.should be_assigned_to(user, child_record)
+  end
+
+  it "propagates roles from grandparent records" do
+    grandchild_record = Record.create!(:parent => child_record)
+
+    role.assign_to(user, parent_record)
+    role.should be_assigned_to(user, grandchild_record)
+  end
+
+  it "propagates roles assigned before the child creation" do
+    role.assign_to(user, parent_record)
+
+    child_record = Record.create!(:parent => parent_record)
+    role.should be_assigned_to(user, child_record)
+  end
+
+  it "propagates the 'unassignment' of roles" do
+    role.assign_to(user, parent_record)
+
+    role.unassign_from(user, parent_record)
+    role.should_not be_assigned_to(user, child_record)
+  end
+end
