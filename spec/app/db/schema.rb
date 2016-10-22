40212188a8ff7ba44bf80aa6f6a797d103232fb9
Alexandre Lenkaster <lenkaster@tn.com.br>
2016-10-22 17:59:27 -0200

    OQGraph data structure undone

--- spec/app/db/schema.rb
+++ spec/app/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160825172308) do
+ActiveRecord::Schema.define(:version => 20161022173834) do
 
   create_table "ac_assignments", :force => true do |t|
     t.integer "role_id",                   :null => false
@@ -38,36 +38,12 @@ ActiveRecord::Schema.define(:version => 20160825172308) do
   add_index "ac_parents", ["child_id"], :name => "index_ac_parents_on_child_id"
   add_index "ac_parents", ["parent_id", "child_id"], :name => "index_ac_parents_on_parent_id_and_child_id", :unique => true
 
-  create_table "ac_paths", :id => false, :force => true do |t|
-    t.string  "latch",  :limit => 32
-    t.integer "origid", :limit => 8
-    t.integer "destid", :limit => 8
-    t.float   "weight"
-    t.integer "seq",    :limit => 8
-    t.integer "linkid", :limit => 8
-  end
-
-  add_index "ac_paths", ["latch", "destid", "origid"], :name => "latch_2"
-  add_index "ac_paths", ["latch", "origid", "destid"], :name => "latch"
-
   create_table "ac_principals", :force => true do |t|
     t.string  "subject_type", :limit => 40, :null => false
     t.integer "subject_id",                 :null => false
   end
 
   add_index "ac_principals", ["subject_type", "subject_id"], :name => "index_ac_principals_on_subject_type_and_subject_id", :unique => true
-
-  create_table "ac_reversed_paths", :id => false, :force => true do |t|
-    t.string  "latch",  :limit => 32
-    t.integer "origid", :limit => 8
-    t.integer "destid", :limit => 8
-    t.float   "weight"
-    t.integer "seq",    :limit => 8
-    t.integer "linkid", :limit => 8
-  end
-
-  add_index "ac_reversed_paths", ["latch", "destid", "origid"], :name => "latch_2"
-  add_index "ac_reversed_paths", ["latch", "origid", "destid"], :name => "latch"
 
   create_table "ac_roles", :force => true do |t|
     t.string "name", :limit => 40, :null => false
